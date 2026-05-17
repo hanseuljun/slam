8eece572ded1b8a6cd864a02fffa43e896ae5335
Hanseul Jun <hanseuljun@gmail.com>
2026-05-16 17:10:30 -0700

    tabs

--- run_visualizer.py
+++ run_visualizer.py
@@ -3,15 +3,23 @@ from imgui_bundle import imgui, immapp, hello_imgui
 
 def gui():
     viewport = imgui.get_main_viewport()
-    imgui.set_next_window_pos(viewport.get_center(), imgui.Cond_.always, (0.5, 0.5))
-    imgui.set_next_window_size((0, 0))
+    imgui.set_next_window_pos(viewport.work_pos)
+    imgui.set_next_window_size(viewport.work_size)
     imgui.begin(
-        "##placeholder",
+        "##main",
         flags=imgui.WindowFlags_.no_title_bar
         | imgui.WindowFlags_.no_resize
-        | imgui.WindowFlags_.no_move,
+        | imgui.WindowFlags_.no_move
+        | imgui.WindowFlags_.no_scrollbar,
     )
-    imgui.text("SLAM Visualizer")
+
+    if imgui.begin_tab_bar("##tabs"):
+        if imgui.begin_tab_item("Overview")[0]:
+            imgui.end_tab_item()
+        if imgui.begin_tab_item("Camera Frames")[0]:
+            imgui.end_tab_item()
+        imgui.end_tab_bar()
+
     imgui.end()
 
 
