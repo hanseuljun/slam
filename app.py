21d42312156726a20280544e82552edffe0c1f83
Hanseul Jun <hanseuljun@gmail.com>
2026-05-17 01:29:46 -0700

    remove main.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from imgui_bundle import imgui
+from pathlib import Path
+
+from imgui_bundle import imgui, hello_imgui, immapp
 
 from slam import DataFolder
 from ui.camera_frames_tab import CameraFramesState, camera_frames_tab
@@ -48,3 +50,20 @@ class App:
             imgui.end_tab_bar()
 
         imgui.end()
+
+
+def main():
+    data = DataFolder.load(Path("data/machine_hall/MH_01_easy/mav0"))
+    app = App(data)
+
+    runner_params = hello_imgui.RunnerParams()
+    runner_params.app_window_params.window_title = "SLAM Visualizer"
+    runner_params.app_window_params.window_geometry.size = (1280, 720)
+    runner_params.ini_filename = "main.ini"
+    runner_params.callbacks.show_gui = app.render
+
+    immapp.run(runner_params)
+
+
+if __name__ == "__main__":
+    main()
