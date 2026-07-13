819fe3a8271a6715f4a0f3b4fbc4af99f7492b22
Hanseul Jun <hanseuljun@gmail.com>
2026-07-12 22:26:21 -0700

    HM_02_easy

--- ui/config_view.py
+++ ui/config_view.py
@@ -8,7 +8,7 @@ from imgui_bundle import imgui
 @dataclass
 class ConfigViewModel:
     data_paths: list[str] = field(default_factory=list)
-    selected_index: int = 0
+    selected_index: int = 1
     start_s: float = 0.0
     duration_s: float = 50.0
     run_coordinate_mapping_check: bool = False
